466d34fe2c1929dbff8990a6734f03658c33d45a
Kinen <romain.racamier@gmail.com>
2015-04-25 11:41:12 +0200

    added priceminister e2e test

---
+++ test/e2e/priceminister-uris-test.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// set the timeout of these tests to 10 seconds
+jasmine.getEnv().defaultTimeoutInterval = 10000;
+
+var PriceFinder = require("../../lib/price-finder"),
+    priceFinder = new PriceFinder();
+
+function verifyPrice(price) {
+    expect(price).toBeDefined();
+    // we can't guarantee the price, so just make sure it's a number
+    // that's more than -1
+    expect(price).toBeGreaterThan(-1);
+}
+
+function verifyName(actualName, expectedName) {
+    expect(actualName).toEqual(expectedName);
+}
+
+function verifyCategory(actualCategory, expectedCategory) {
+    expect(actualCategory).toEqual(expectedCategory);
+}
+
+describe("price-finder for PriceMinister Store URIs", function() {
+
+    // Video Games
+    describe("testing a Video Game item", function() {
+
+        // Grand Theft Auto IV sur PS3
+        var uri = "http://www.priceminister.com/mfp/2001701/gta-4-jeu-video#pid=63080574";
+
+        it("should respond with a price for findItemPrice()", function(done) {
+            priceFinder.findItemPrice(uri, function(err, price) {
+                expect(err).toBeNull();
+                verifyPrice(price);
+                done();
+            });
+        });
+
+        it("should respond with a price, and the right category and name for findItemDetails()", function(done) {
+            priceFinder.findItemDetails(uri, function(err, itemDetails) {
+                expect(err).toBeNull();
+                expect(itemDetails).toBeDefined();
+
+                verifyPrice(itemDetails.price);
+                verifyName(itemDetails.name, "Grand Theft Auto IV sur PS3");
+                verifyCategory(itemDetails.category, "Video Games");
+
+                done();
+            });
+        });
+    });
+});
